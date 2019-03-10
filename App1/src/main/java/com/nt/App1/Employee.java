2592d45a5e88788e08d37dc0a21c47d658a4ab3f
Mahesh <maheshgoud419@gmail.com>
2019-03-10 22:12:19 +0530

    1st commit

---
+++ App1/src/main/java/com/nt/App1/Employee.java
@@ -0,0 +1,18 @@
+package com.nt.App1;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+int id;
+String name;
+int age;
+public Employee(int id, String name, int age) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.age = age;
+}
+
+
+}
